407b8b1e3c9826e1f72d858ecb223dadd393b3e9
Andry Carolay Camacho Martinez <acarolaycamacho@unicesar.edu.co>
2026-02-27 15:32:08 -0500

    feat: crear clase estudiante con sus atributos

---
+++ src/main/java/com/mycompany/sistemaacademico/estudiante.java
@@ -0,0 +1,18 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.mycompany.sistemaacademico;
+
+/**
+ *
+ * @author jadis
+ */
+public class estudiante {
+    private String codigo;
+    private String nombre;
+    private String apellido;
+    private int edad;
+    private int semestre;
+    
+}
