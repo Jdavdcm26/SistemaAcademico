b72a3bb83182ebe47eb7d965dbae9d861532aab2
Angel Areiza <aaareiza@gmail.com>
2026-02-28 14:40:49 -0500

    cree la clase Asignatura y sus atributos

---
+++ src/main/java/com/mycompany/sistemaacademico/Asignatura.java
@@ -0,0 +1,17 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.mycompany.sistemaacademico;
+
+/**
+ *
+ * @author angel
+ */
+public class Asignatura {
+    private String codigo;
+    private String nombre;
+    private int creditos;
+    private String docente;
+      
+}
