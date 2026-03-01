ef00aa7a2ef3cac1ed34fa0339436de7c37be6a7
Jose Castro Matos <josedavidcastro@unicesar.edu.co>
2026-03-01 08:26:15 -0500

    feat: crear estructura base de Main con ArrayLists

---
+++ src/main/java/com/mycompany/sistemaacademico/Main.java
@@ -0,0 +1,19 @@
+
+package com.mycompany.sistemaacademico;
+
+import java.util.ArrayList;
+import java.util.Scanner;
+
+
+
+public class Main {
+    
+        ArrayList <estudiante> estudiantes= new ArrayList<>();
+        ArrayList <Asignatura> Asignaturas= new ArrayList<>();
+        ArrayList <Nota> notas= new ArrayList<>();
+        Scanner sc = new Scanner(System.in);
+    
+    public static void main(String[] args) {
+        
+    }
+}
