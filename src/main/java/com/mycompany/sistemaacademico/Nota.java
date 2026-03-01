35678ed0c2d837c211a5f59569d56b038c332443
Angel Areiza <aaareiza@gmail.com>
2026-03-01 01:56:32 -0500

    feat: crear clase Nota con atributos

---
+++ src/main/java/com/mycompany/sistemaacademico/Nota.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.mycompany.sistemaacademico;
+
+/**
+ *
+ * @author angel
+ */
+public class Nota {
+    private estudiante estudiate; //cambiar nombre de clase a "Estudiante" -> e = E
+    private Asignatura asignatura;
+    private double Valor;
+    private String periodo;  
+}
